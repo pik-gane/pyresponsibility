641d6d30d27cde7a227d788ea7e06f8aced0e833
Jobst Heitzig <heitzig@pik-potsdam.de>
2021-12-06 15:32:24 +0100

    docstrings for functions

--- src/responsibility/functions.py
+++ src/responsibility/functions.py
@@ -2,6 +2,8 @@ from .core import _AbstractObject
 
 
 class Function (_AbstractObject):
+    """Represents any function. Parent class for the classes below"""
+    
     _i_function = None
     def __init__(self, function):
         assert hasattr(function, "__call__"), "must supply a function"
@@ -9,12 +11,20 @@ class Function (_AbstractObject):
 
 
 class AggregationFunction (Function):
+    """Represents an aggregation function taking any number of values and
+    returning an "aggregate" of some kind, such as the min, max, mean, or
+    sum."""
+    
     def __call__(self, values):
         """values: iterable of numbers"""
         return self._i_function(values)
         
         
-class ResponsibilityFunction (_AbstractObject):
+class ResponsibilityFunction (Function):
+    """Represents a responsibility function taking a tree, a group, and a node
+    and returning an assessment of that group's pointwise, backward, or forward
+    responsibility at that node.""" 
+    
     def __call__(self, tree=None, group=None, node=None):
         return self._i_function(tree=None, group=None, node=None)
         
